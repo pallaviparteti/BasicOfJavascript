80cc7d3068badc3b3f31451ac60c60637c1ee4c0
pallaviparteti <pallaviparteti2000@gmail.com>
2023-08-29 19:22:21 +0530

    [pallavi]Added:javascript variables

---
+++ dataTypes.js
@@ -0,0 +1,53 @@
+// JavaScript has 8 Datatypes
+
+// String
+// Number
+// Bigint
+// Boolean
+// Undefined
+// Null
+// Symbol
+// Object 
+//---------OBJECT----------------
+Examples
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25"); 
+
+// JavaScript Types are Dynamic
+
+// JavaScript has dynamic types. 
+// This means that the same variable can be used to hold different data types:
+// Example
+let a;       // Now x is undefined
+a = 5;       // Now x is a Number
+a = "John";  // Now x is a String 
+
+// JavaScript Strings
+
+// A string or a text string is a series of characters like "John Doe".
+
+// Strings are written with quotes. We can use single or double quotes:
+Example
+// Using double quotes:
+let carName1 = "Volvo XC60";
+
+// Using single quotes:
+let carName2 = 'Volvo XC60';
