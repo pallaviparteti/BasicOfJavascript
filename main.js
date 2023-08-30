f80e8cf30095d1a7c78135203fdebe70255f88c0
pallaviparteti <pallaviparteti2000@gmail.com>
2023-08-30 18:27:12 +0530

    [pallavi]Added:modules with import export examples

---
+++ main.js
@@ -0,0 +1,9 @@
+// main.js
+import { add, subtract } from './mainModel.js';
+
+console.log(add(5, 3));       // Output: 8
+console.log(subtract(10, 4)); // Output: 6
+
+// Importing from a Module
+// In another file, you can import functions from the math.js module
+
---
+++ module.js
@@ -0,0 +1,10 @@
+//modules
+// Modules in JavaScript are a way to organize code into separate files, 
+// making it easier to manage and maintain complex applications.
+// Modules allow you to encapsulate functionality and expose specific parts for other modules to use.
+
+// the export and import statements are used to work with modules. 
+// Modules allow you to organize your code into separate files and share 
+// functionality between different parts of your application. 
+// The export statement is used to define what parts of a module should be accessible from the outside,
+//  and the import statement is used to bring those exported parts into another module.
